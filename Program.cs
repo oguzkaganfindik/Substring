1a1731b09ebd0aa327053cd6e81e852b67290d58
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-05-05 13:27:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AltMetin
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string orijinalMetin = "Hello World!";
+            int baslangicIndex = 6;
+            int uzunluk = 6;
+            string altMetin = AltMetinGetir(orijinalMetin, baslangicIndex, uzunluk);
+            Console.WriteLine(altMetin);
+        }
+
+        static string AltMetinGetir(string metin, int baslangicIndex, int uzunluk)
+        {
+            char[] sonuc = new char[uzunluk];
+
+            int i = 0;
+            while (i < uzunluk && baslangicIndex + i < metin.Length)
+            {
+                sonuc[i] = metin[baslangicIndex + i];
+                i++;
+            }
+
+            return new string(sonuc, 0, i);
+        }
+    }
+}
+
+
+
+
+
+
+
